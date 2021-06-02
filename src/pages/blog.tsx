bac4308d7f8af484a375aae45c74a2152d986f57
Walisson Silva <walissonsilva10@gmail.com>
2021-06-02 20:41:12 -0300

    chore: add search bar to find posts on blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,18 +1,22 @@
 import { GetStaticProps } from "next"
+import { BiSearch } from "react-icons/bi"
+
 import { Header } from "../components/Header"
 import NextHead from "../components/NextHead"
-import { SectionTitle } from "../styles/atoms"
-import { SectionContainer } from "../styles/containers"
-
-import { getPostsToBlogPage } from '../services/prismic';
-
-import {
-  Container,
-  PostsContainer,
-} from '../styles/pages/blog';
 import { IBlogCard } from "../components/HomeSections"
 import { BlogCard } from "../components/BlogCard"
 import Footer from "../components/Footer"
+import { getPostsToBlogPage } from '../services/prismic';
+
+import { SectionContainer } from "../styles/containers"
+import {
+  Container,
+  PostsContainer,
+  FlexContainer,
+  SearchBarContainer,
+  SearchInput,
+} from '../styles/pages/blog';
+import { SectionTitle } from "../styles/atoms"
 
 interface BlogProps {
   posts: IBlogCard[];
@@ -30,10 +34,20 @@ export default function Blog({
       <SectionContainer>
         
         <Container>
-          <SectionTitle>
-            Blog
-            <hr />
-          </SectionTitle>
+          <FlexContainer>
+            <SectionTitle>
+              Blog
+              <hr />
+            </SectionTitle>
+
+            <SearchBarContainer>
+              <SearchInput
+                placeholder="Pesquisa..."
+              />
+              <BiSearch />
+            </SearchBarContainer>
+          </FlexContainer>
+
 
           <PostsContainer>
             { posts.map(post => (
--- src/styles/pages/blog.ts
+++ src/styles/pages/blog.ts
@@ -19,3 +19,47 @@ export const PostsContainer = styled.div`
     grid-template-columns: 1fr;
   }
 `
+
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`
+
+export const SearchBarContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding: 0.7rem 1rem;
+  border-radius: 10px;
+  background-color: ${props => props.theme.colors["background-primary"]};
+
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  svg {
+    font-size: 1.1rem;
+  }
+
+  @media (max-width: 600px) {
+    margin-top: 2rem;
+    max-width: 100%;
+  }
+`
+
+export const SearchInput = styled.input`
+  background-color: transparent;
+  color: ${props => props.theme.colors.white};
+  font-size: 1rem;
+  width: 100%;
+  height: 1.25rem;
+
+  &::placeholder {
+    color: ${props => props.theme.colors.alpha};
+  }
+`
